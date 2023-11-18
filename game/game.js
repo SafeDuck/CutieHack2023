a44366db4b652de9bc00e7d6d0cdb30ebcc05d34
Alan <alanxuzhang@yahoo.com>
2023-11-18 15:49:59 -0800

    Added 2nd player

--- game/game.js
+++ game/game.js
@@ -1,122 +1,128 @@
-var player = {
-    x: 200,
-    y: 200,
-    x_v: 0,
-    y_v: 0,
-    jump: true,
-    height: 20,
-    width: 20
-};
+var player1 = createPlayer(200, 200, "#F08080");
+var player2 = createPlayer(100, 200, "#80F080");
 
 var keys = {
-    right: false,
     left: false,
+    right: false,
     up: false,
+    a: false,
+    d: false,
+    w: false,
+    s: false,
 };
 
 var gravity = 0.6;
 var friction = 0.7;
-var num = 2;
+var jumpStrength = 10;
+var numPlatforms = 2;
 var platforms = [];
+
+function createPlayer(x, y, color) {
+    return {
+        x: x,
+        y: y,
+        x_v: 0,
+        y_v: 0,
+        jump: false,
+        height: 20,
+        width: 20,
+        color: color,
+    };
+}
+
+function createPlatforms() {
+    for (let i = 0; i < numPlatforms; i++) {
+        platforms.push({
+            x: 100 * i,
+            y: 200 + 30 * i,
+            width: 110,
+            height: 15,
+        });
+    }
+}
 
 function renderCanvas() {
     ctx.fillStyle = "#F0F8FF";
     ctx.fillRect(0, 0, 270, 270);
 }
 
-function renderPlayer() {
-    ctx.fillStyle = "#F08080";
+function renderPlayer(player) {
+    ctx.fillStyle = player.color;
     ctx.fillRect(player.x - 20, player.y - 20, player.width, player.height);
-}
-
-function createPlatforms() {
-    for (let i = 0; i < num; i++) {
-        platforms.push({
-            x: 100 * i,
-            y: 200 + 30 * i,
-            width: 110,
-            height: 15
-        });
-    }
 }
 
 function renderPlatforms() {
     ctx.fillStyle = "#45597E";
-    ctx.fillRect(platforms[0].x, platforms[0].y, platforms[0].width, platforms[0].height);
-    ctx.fillRect(platforms[1].x, platforms[1].y, platforms[1].width, platforms[1].height);
+    platforms.forEach(platform => {
+        ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+    });
 }
 
 function keyDown(e) {
-    if (e.keyCode == 37) {
-        keys.left = true;
-    }
-    if (e.keyCode == 38) {
-        if (player.jump == false) {
-            player.y_v = -10;
-        }
-    }
-    if (e.keyCode == 39) {
-        keys.right = true;
-    }
+    if (e.key === "ArrowLeft") keys.left = true;
+    if (e.key === "ArrowRight") keys.right = true;
+    if (e.key === "ArrowUp") keys.up = true;
+    if (e.key === "a") keys.a = true;
+    if (e.key === "d") keys.d = true;
+    if (e.key === "w") keys.w = true;
+    if (e.key === "s") keys.s = true;
 }
 
 function keyUp(e) {
-    if (e.keyCode == 37) {
-        keys.left = false;
-    }
-    if (e.keyCode == 38) {
-        if (player.y_v < -2) {
-            player.y_v = -3;
-        }
-    }
-    if (e.keyCode == 39) {
-        keys.right = false;
-    }
+    if (e.key === "ArrowLeft") keys.left = false;
+    if (e.key === "ArrowRight") keys.right = false;
+    if (e.key === "ArrowUp") keys.up = false;
+    if (e.key === "a") keys.a = false;
+    if (e.key === "d") keys.d = false;
+    if (e.key === "w") keys.w = false;
+    if (e.key === "s") keys.s = false;
 }
 
 function gameLoop() {
-    if (player.jump == false) {
+    updatePlayer(player1, keys.left, keys.right, keys.up);
+    updatePlayer(player2, keys.a, keys.d, keys.w);
+    renderCanvas();
+    renderPlayer(player1);
+    renderPlayer(player2);
+    renderPlatforms();
+}
+
+function updatePlayer(player, leftKey, rightKey, upKey) {
+    if (player.jump === false) {
         player.x_v *= friction;
     } else {
         player.y_v += gravity;
     }
-    player.jump = true;
 
-    if (keys.left) {
+    // Allow jumping only if player is on the ground
+    if (upKey && !player.jump) {
+        player.y_v = -jumpStrength;
+        player.jump = true;
+    }
+
+    // Horizontal movement
+    if (leftKey) {
         player.x_v = -2.5;
     }
-    if (keys.right) {
+    if (rightKey) {
         player.x_v = 2.5;
     }
 
     player.y += player.y_v;
     player.x += player.x_v;
 
-    let i = -1;
-    if (
-        platforms[0].x < player.x &&
-        player.x < platforms[0].x + platforms[0].width &&
-        platforms[0].y < player.y &&
-        player.y < platforms[0].y + platforms[0].height
-    ) {
-        i = 0;
-    }
-    if (
-        platforms[1].x < player.x &&
-        player.x < platforms[1].x + platforms[1].width &&
-        platforms[1].y < player.y &&
-        player.y < platforms[1].y + platforms[1].height
-    ) {
-        i = 1;
-    }
-    if (i > -1) {
-        player.jump = false;
-        player.y = platforms[i].y;
-    }
-
-    renderCanvas();
-    renderPlayer();
-    renderPlatforms();
+    // Collision with platforms
+    platforms.forEach(platform => {
+        if (
+            player.x > platform.x &&
+            player.x < platform.x + platform.width &&
+            player.y > platform.y &&
+            player.y < platform.y + platform.height
+        ) {
+            player.jump = false;
+            player.y = platform.y;
+        }
+    });
 }
 
 var canvas = document.getElementById("canvas");
